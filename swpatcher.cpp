ddb00fc0196b3bb46e74380fc0f578fa4562e4ba
Bordei Tudor <tudor14x@gmail.com>
2026-09-05 12:02:28 +0300

    fix .git files being copied over when patching a game

--- swpatcher.cpp
+++ swpatcher.cpp
@@ -10,6 +10,11 @@
 
 namespace fs = std::filesystem;
 
+std::string fixPath(const std::string &strPath)
+{
+    return fs::path(strPath).make_preferred().string();
+}
+
 std::string executableName;
 #define DEFAULT_SUFFIX "_patched"
 
@@ -23,7 +28,8 @@ void help(int code = 0, const char *format = NULL, ...)
     std::cout << "\tsuffix                 (string)   = name appended to the end of a directory to mark it as to be copied; default value is `" << DEFAULT_SUFFIX << "`\n\n";
     std::cout << executableName << " works by copying the `steamapps/workshop/content/<appid>[suffix]` directory into `steamapps/workshop/content/[appid]` and then launching the game; this is useful for when you make local changes to workshop content and don't want them to be overriden the next time you open the game.\n\n";
 
-    if(!format) {
+    if (!format)
+    {
         std::cout << "\n\n";
         exit(0);
     }
@@ -33,9 +39,12 @@ void help(int code = 0, const char *format = NULL, ...)
 
     char buf[255];
     auto ec = vsprintf(buf, format, args);
-    if(ec < 0) {
+    if (ec < 0)
+    {
         std::cerr << "formatting error\n";
-    } else {
+    }
+    else
+    {
         std::cout << buf;
     }
 
@@ -50,8 +59,7 @@ const std::string argNames[] = {
     "appid",
     "absoluteWorkshopPath",
     "absoluteExecutablePath",
-    "suffix"
-};
+    "suffix"};
 
 /* to prevent duplicates */
 std::set<std::string> argsUsed = {};
@@ -64,7 +72,8 @@ int main(int argc, char **argv)
     argc -= 1;
     argv++;
 
-    if(argc < 2) {
+    if (argc < 2)
+    {
         help();
     }
 
@@ -72,50 +81,98 @@ int main(int argc, char **argv)
     short absolutePathsSet = 0;
     long long appid = 0;
 
-    for(int i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i)
+    {
         std::string arg(argv[i]);
-        if(arg.substr(0, 2) != "--") help(-1, "error: invalid argument format: missing `--`");
-        arg.assign(arg.data()+2);
+        if (arg.substr(0, 2) != "--")
+            help(-1, "error: invalid argument format: missing `--`");
+        arg.assign(arg.data() + 2);
         size_t eqpos = arg.find('=');
-        if(eqpos == std::string::npos) help(-1, "error: invalid argument format: missing `=` after argument name or `--`\n");
+        if (eqpos == std::string::npos)
+            help(-1, "error: invalid argument format: missing `=` after argument name or `--`\n");
         std::string argName = arg.substr(0, eqpos);
-        if(argName.size() == 0) help(-1, "error: invalid argument format: argument name is empty\n");
-        if(std::find(std::begin(argNames), std::end(argNames), argName) == std::end(argNames)) help(-1, "Invalid argument `%s`\n", argName.c_str());
-        arg.assign(arg.data()+argName.size()+1);
+        if (argName.size() == 0)
+            help(-1, "error: invalid argument format: argument name is empty\n");
+        if (std::find(std::begin(argNames), std::end(argNames), argName) == std::end(argNames))
+            help(-1, "Invalid argument `%s`\n", argName.c_str());
+        arg.assign(arg.data() + argName.size() + 1);
         std::string argValue = arg;
 
-        if(argsUsed.count(argName) != 0) {
+        if (argsUsed.count(argName) != 0)
+        {
             std::cerr << "error: duplicate argument `" << argName << "`\n";
             return -1;
         }
         argsUsed.insert(argName);
 
-    
-        if(argName == "executablePath") {
+        if (argName == "executablePath")
+        {
             executablePath = argValue;
-        } else if(argName == "appid") {
+        }
+        else if (argName == "appid")
+        {
             appid = atoll(argValue.c_str());
-        } else if(argName == "absoluteExecutablePath") {
+        }
+        else if (argName == "absoluteExecutablePath")
+        {
             executablePath = argValue;
-        } else if(argName == "absoluteWorkshopPath") {
+        }
+        else if (argName == "absoluteWorkshopPath")
+        {
             workshopPath = argValue;
-        } else if(argName == "suffix") {
+        }
+        else if (argName == "suffix")
+        {
             suffix = argValue;
         }
     }
 
-    if(!fs::exists(fs::path(executablePath))) {
+    if (!fs::exists(fs::path(executablePath)))
+    {
         std::cerr << "error: no such file or directory `" << executablePath << "` exists\n";
         return -1;
     }
 
-    if(workshopPath.empty()) {
-        workshopPath = std::filesystem::current_path().parent_path().parent_path().string() + "/workshop/content";
+    if (workshopPath.empty())
+    {
+        workshopPath = fixPath(std::filesystem::current_path().parent_path().parent_path().string() + "\\workshop\\content");
     }
-    auto originalPath = workshopPath + "/" + std::to_string(appid);
-    auto patchedPath = originalPath + suffix;
+    auto originalPath = fixPath(workshopPath + "\\" + std::to_string(appid));
+    auto patchedPath = fixPath(originalPath + suffix);
 
-    fs::copy(patchedPath, originalPath, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
+    namespace fs = std::filesystem;
+
+    fs::recursive_directory_iterator it(patchedPath);
+    fs::recursive_directory_iterator end;
+
+    for (; it != end; ++it)
+    {
+        const auto &entry = *it;
+
+        // Skip .git and everything inside it
+        if (entry.is_directory() && entry.path().filename() == ".git")
+        {
+            it.disable_recursion_pending();
+            continue;
+        }
+
+        auto relative = fs::relative(entry.path(), patchedPath);
+        auto target = originalPath / relative;
+
+        if (entry.is_directory())
+        {
+            fs::create_directories(target);
+        }
+        else if (entry.is_regular_file())
+        {
+            fs::create_directories(target.parent_path());
+
+            fs::copy_file(
+                entry.path(),
+                target,
+                fs::copy_options::overwrite_existing);
+        }
+    }
 
     executablePath = "\"" + executablePath + "\"";
 
